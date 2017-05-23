6b6b6d6c7e1b26a58fbb7aa47ebb8e5892103e8c
Paul Charles <PCharles71@RocketMail.com>
2017-05-23 13:19:35 -0800

    Updated .css .js .html files to make a proper Todo List.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,25 @@
-alert("conected");
+// Check off specific Todos by clicking
+$('ul').on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todos
+$("ul").on("click", "span", function(event) {
+  $(this).parent().fadeOut(500, function() { // this = <span>
+    $(this).remove();  // this = <li>
+  });
+  event.stopPropagation(); // keeps this from running on all parents
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) { // 13 is keycode ENTER
+      var todoText = $(this).val();
+      $(this).val("");
+      //create a new li and add to ul
+      $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+  }
+});
+
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle()
+});
